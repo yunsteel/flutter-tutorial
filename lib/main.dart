96fda61a440606a00734710b6a2aeb952959562a
yunsteel <archer0082@naver.com>
2024-12-20 15:10:11 +0900

    chore: first commit

---
+++ lib/main.dart
@@ -0,0 +1,114 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// StatelessWidget, 빌드하는 역할
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+// 값이 변경됐을 때 다른 위젯이 알아야 함. through ChangeNotifier
+// React의 Context와 비슷한 것 같음.
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+
+  var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  // 모든 위젯은 build() 내 상황이 바뀔 때 마다 최신 상태 유지함
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>(); // watch로 변경사항 추적함
+    var pair = appState.current;
+
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    // 위젯은 중첩된 트리를 반환해야됨. 이때 최상위가 Scaffold
+    return Scaffold(
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                  icon: Icon(icon),
+                  onPressed: appState.toggleFavorite,
+                  label: Text("좋아요")),
+              SizedBox(width: 10),
+              ElevatedButton(onPressed: appState.getNext, child: Text("Next"))
+            ],
+          )
+        ],
+      )),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({super.key, required this.pair});
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    // 현재 앱의 테마
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium?.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text(
+            pair.asLowerCase,
+            style: style,
+            semanticsLabel: "${pair.first} ${pair.second}",
+          )),
+    );
+  }
+}
